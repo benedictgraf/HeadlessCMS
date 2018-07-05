47d03e1cfbb9fce0718bc9d804eeab25f051197b
Benedict Graf <benedict.graf@gmail.com>
2018-07-05 15:09:21 +0200

    no message

--- auth.controller.js
+++ auth.controller.js
@@ -1,19 +1,13 @@
-//auth.controller.js is responsible for login process to /admin panel and checks if the credentials fit the firebase-users credentials
-
 app.controller("AuthCtrl", ["$scope", "$firebaseAuth", "$rootScope", '$state',
   function($scope, $firebaseAuth, $rootScope, $state) {
     $scope.login = function(){
     $scope.authObj = $firebaseAuth();
     var email = document.getElementById('email').value;
     var pass = document.getElementById('password').value;
-    $scope.editmode = false;
 
     $scope.authObj.$signInWithEmailAndPassword(email, pass).then(function(firebaseUser) {
         console.log("Signed in as:", firebaseUser.uid);
-        $state.go("view")
-        $scope.toggleEditMode = function(){
-          $scope.editmode = $scope.editmode === false ? true: false;
-        }
+        $state.go("panel")
       }).catch(function(error) {
         console.error("Authentication failed:", error);
       });
@@ -36,3 +30,82 @@ app.controller("AuthCtrl", ["$scope", "$firebaseAuth", "$rootScope", '$state',
    }
   }
 ]);
+
+/*
+app.controller("AuthCtrl", ["$scope", "$firebaseAuth",
+  function($scope, $firebaseAuth) {
+    var auth = $firebaseAuth();
+
+    $scope.signIn = function() {
+      $scope.firebaseUser = null;
+      $scope.error = null;
+
+      auth.$signInAnonymously().then(function(firebaseUser) {
+        $scope.firebaseUser = firebaseUser;
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+    };
+  }
+]);
+*/
+
+/*
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    return $firebaseAuth();
+  }
+]);
+
+app.controller('AuthCtrl', ['$scope', '$state', '$http', 'Auth',
+  function($scope, $state, $http, Auth) {
+    $scope.auth = Auth;
+
+
+
+    $scope.login = function(email: string, password: string) {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        console.log("mail:" + email +  " " + "pass:" + password)
+      Auth.$signInWithEmailAndPassword({
+        email: '',
+        password: ''
+      })
+      .then(function(authData) {
+        console.log('Logged in as:', authData.uid);
+        //$state.go('profile');
+      })
+      .catch(function(err) {
+        console.log('error:',err);
+        //$state.go('login');
+      });
+    };
+  }
+]);
+/*
+
+/*
+// let's create a re-usable factory that generates the $firebaseAuth instance
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    return $firebaseAuth();
+  }
+]);
+
+// and use it in our controller
+app.controller("AuthCtrl", ["$scope", "Auth",
+  function($scope, Auth) {
+    $scope.login = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      // Create a new user
+      Auth.$authWithPassword($scope.email, $scope.password)
+        .then(function(firebaseUser) {
+          $scope.message = "User created with uid: " + firebaseUser.uid;
+        }).catch(function(error) {
+          $scope.error = error;
+        });
+    };
+}]);
+*/
--- showCaseCtrl.js
+++ showCaseCtrl.js
@@ -1,7 +1,4 @@
-//Initialize angularJS app with firebase and uiRouter
 var app = angular.module('mainApp', ['ui.router', 'firebase']);
-
-//Define States/Sites for uiRouter
 app.config(function($stateProvider, $urlRouterProvider){
     $stateProvider
     .state('view', {
@@ -19,31 +16,34 @@ app.config(function($stateProvider, $urlRouterProvider){
       $urlRouterProvider.otherwise('/view');
 })
 
-//showCaseCtrl is responsible for displaying
+
 app.controller('showCaseCtrl', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray) {
 
-    //create a firebase references with ('a name')
+    // CREATE A FIREBASE REFERENCE
     var showCaseRef = firebase.database().ref('posts');
     var viewRef = firebase.database().ref('view');
 
 
-    //get entries as an array from previous created refs
+    // GET Entries AS AN ARRAY
     $scope.entries = $firebaseArray(showCaseRef);
     $scope.viewEntries = $firebaseArray(viewRef);
 
-    //get value (true) of currently clicked entry
-    $scope.setActive = function (event){
+    // GET Value of currently clicked Entry
+    $scope.setActive = function (event)
+    {
+    //console.log(event.target.innerHTML);
     var viewItem = event.target.innerHTML;
+    //console.log("viewItem: " + viewItem);
     firebase.database().ref().child('view/active').set({data: event.target.innerHTML});
     };
     }]);
 
-//pushCtrl is responsible for writing new posts into firebase
+
 app.controller('pushCtrl', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray) {
 
-    //get value of input-field and write as new Post into DB
+    // Get Value of Input -> Write new Post into DB
     $scope.writeNewPost = function()
     {firebase.database().ref().child('posts').push({data: $scope.insertNew});
     };
